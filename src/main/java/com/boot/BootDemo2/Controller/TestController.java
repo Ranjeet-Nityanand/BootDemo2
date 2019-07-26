abec36f7f8e9e133ec2fbd2ad3f25d1af73a504b
Ranjeet <ranjeetnityanand@gmail.com>
2019-07-26 18:52:44 +0530

    add hello world message

--- src/main/java/com/boot/BootDemo2/Controller/TestController.java
+++ src/main/java/com/boot/BootDemo2/Controller/TestController.java
@@ -3,7 +3,12 @@
  */
 package com.boot.BootDemo2.Controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Ranjeet
@@ -12,4 +17,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestController {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+
+	@RequestMapping(value = "/")
+	@ResponseBody
+	public String helloMessage() {
+		logger.info("info message-------------======-");
+		logger.warn("Warning Message================");
+		logger.error("Error Message=================");
+		return "Hello World";
+	}
 }
