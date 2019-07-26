51f501f93bbe6d82aa9a83c3096ee6724ad272ed
Ranjeet <ranjeetnityanand@gmail.com>
2019-07-26 17:10:03 +0530

    2nd test

--- src/main/java/com/boot/BootDemo2/Controller/TestController.java
+++ src/main/java/com/boot/BootDemo2/Controller/TestController.java
@@ -3,10 +3,13 @@
  */
 package com.boot.BootDemo2.Controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author Ranjeet
  *
  */
+@Controller
 public class TestController {
 
 }
