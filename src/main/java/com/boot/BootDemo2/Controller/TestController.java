73882aa8b51f02a32b353cd4e3dde515d34d0cfd
Ranjeet <ranjeetnityanand@gmail.com>
2019-08-20 10:48:22 +0530

    dndke

--- src/main/java/com/boot/BootDemo2/Controller/TestController.java
+++ src/main/java/com/boot/BootDemo2/Controller/TestController.java
@@ -7,8 +7,11 @@ package com.boot.BootDemo2.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Ranjeet
@@ -17,14 +20,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-	Logger logger = LoggerFactory.getLogger(getClass());
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@RequestMapping(value = "/")
-	@ResponseBody
-	public String helloMessage() {
-		logger.info("info message-------------======-");
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String helloMessage(ModelMap model) {
+		logger.info("info message-------------======");
 		logger.warn("Warning Message================");
-		logger.error("Error Message=================");
-		return "Hello World";
+		// model.put("name", name);
+		// logger.error("Error Message==================" + name);
+		return "login";
 	}
-}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String showMessage(ModelMap model, @RequestParam String name, @RequestParam String password) {
+		model.put("name", name);
+		model.put("password", password);
+		return "welcome";
+	}
+	
+	@RequestMapping(value="/login")
+	public ModelAndView helloMessage(@RequestParam String name) {
+		ModelAndView view = new ModelAndView();
+		view.setViewName("login");
+		view.addObject("name", name);
+		return view;
+	}
+	
+	}
